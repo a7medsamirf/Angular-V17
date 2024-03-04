3ba0bdbb7e0eb7cf0a000e0402a7dbdd36bb2e5b
ahmed samir <ahmed.samir.farid@gmail.com>
2024-03-04 15:20:45 +0200

    V 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { ButtonModule } from 'primeng/button';
 import { TodoComponent } from './Components/todo/todo.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    FooterComponent,
+    HeaderComponent,
     RouterOutlet,
     MatButtonModule,
     ButtonModule,
